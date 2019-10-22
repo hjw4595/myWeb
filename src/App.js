91bc69a9cbbd3819d54edb5f4cefd51df5e682f3
hjw4595 <hongjung73@gmail.com>
2019-10-22 12:33:37 +0900

    input validator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
 
-const useInput = initialValue => {
+const useInput = (initialValue,validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = event => {
     const {
       target: { value }
     } = event;
-    setValue(value);
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };  
   return { value, onChange };
 };
 const App =() => {
-  const name = useInput("")
+  const maxLen = value => value.length < 10 
+  const noAt = value => !value.includes("@");
+  const name = useInput("",maxLen,noAt)
+
   const [item, setItem] = useState(1);
   const incrementItem = () => setItem(item + 1);
   const decrementItem = () => setItem(item - 1);
   return (
     <div>
-      <input placeholder="Hello" onChange={name.onChange} />
+      <input placeholder="Hello" {...name} />
       <h1>{item}</h1>
       <button onClick={incrementItem}>+</button>
       <button onClick={decrementItem}>-</button>
