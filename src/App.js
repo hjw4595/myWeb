04a773328be600dba6c5b9d29f2caa77f109200f
hjw4595 <hongjung73@gmail.com>
2019-10-22 11:13:50 +0900

    useinput

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 
-function App() {
-  const [item, setItoem] = useState(1);
-  const incrementItem = () => setItoem(item + 1);
-  const decrementItem = () => setItoem(item - 1);
+const useInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target: { value }
+    } = event;
+    setValue(value);
+  };  
+  return { value, onChange };
+};
+const App =() => {
+  const name = useInput("")
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
   return (
     <div>
+      <input placeholder="Hello" onChange={name.onChange} />
       <h1>{item}</h1>
       <button onClick={incrementItem}>+</button>
       <button onClick={decrementItem}>-</button>
