1fb207fe836ac0c8246c9b7662a78b693893010f
hjw4595 <hongjung73@gmail.com>
2019-10-29 22:14:21 +0900

    confirm type onclick /add tabimage

--- src/App.js
+++ src/App.js
@@ -4,25 +4,29 @@ import useClick from "./function/useEffect/useClick/index"
 import useInput from "./function/useInput/index"
 import useTab from "./function/useTabs/index"
 
-
 const content = [
   {
     tab: "Section1",
-    content: "Section1"
+    content: "주제1",
+    image: "https://cdn.pixabay.com/photo/2016/09/29/10/08/halloween-1702531_960_720.jpg"
   },
   {
     tab: "Section2",
-    content: "Section2"
+    content: "주제2",
+    image:"https://cdn.pixabay.com/photo/2019/10/13/14/33/flowesr-4546245_1280.jpg"
   }
 ];
 const App =() => {
+  //Title
   const titleUpdate = useTitle("Loading");
   setTimeout(() => titleUpdate("HOME"), 5000)
-
+  //Click
   const clicked = () => {
-    console.log("CLICKED")
+    alert("안녕하세요")
+    document.getElementById("demo").innerHTML = "눌러짐";
   }
   const clickUpdate = useClick(clicked);
+
   //tab
   const {currentItem, changeItem} = useTab(0,content);
   //input
@@ -33,14 +37,19 @@ const App =() => {
 
   return (
     <div>
+
     <div>
       <input placeholder="Hello" {...name} />
-      <h1 ref={ clickUpdate }>i want Click for you</h1>
+      <h1 id="demo" ref={ clickUpdate }>i want Click for you</h1>
     </div>
+
     <div>
       {content.map((section,index) =>(<button onClick={()=> changeItem(index)} >{section.tab}</button>))}
       <div>{currentItem.content}</div>
+      <img alt="1" src={currentItem.image} width="200" height="200"/>
+      
     </div>
+
     </div>
   
   );
--- src/function/useEffect/useClick/useClick.js
+++ src/function/useEffect/useClick/useClick.js
@@ -1,16 +1,20 @@
-import { useEffect , useRef } from "react";
+import { useRef, useEffect } from "react";
 
 export const useClick = onClick => {
-    const element = useRef();
-    useEffect(() => {
-        if (element.current) {
-          element.current.addEventListener("click", onClick);
-        }
-        return () => {
-          if (element.current) {
-            element.current.removeEventListener("click", onClick);
-          }
-        };
-      }, []);
-    return element;
-  };
+  const element = useRef();
+  useEffect(() => {
+    if (typeof onClick === "function") {
+      if (element.current) {
+        element.current.addEventListener("click", onClick);
+      }
+    }
+    return () => {
+      if (element.current) {
+        // eslint-disable-next-line
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+    // eslint-disable-next-line
+  },[]);
+  return element; 
+};
