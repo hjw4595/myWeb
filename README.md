ed5702bdc2b66f4f11d297662e0a78dbce07de0f
hjw4595 <hongjung73@gmail.com>
2019-10-30 13:54:42 +0900

    confirm()/PreventLeave

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ hooks = react의 state에 연결하는 방법 => class를 사용하지 않고 �
 4.useEffect
 5.useTitle
 6.useClick
+-add image 
+err 로컬 이미지를 불러오지 못함
--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import useTitle from "./function/useEffect/useTitle/index"
 import useClick from "./function/useEffect/useClick/index"
 import useInput from "./function/useInput/index"
 import useTab from "./function/useTabs/index"
+import useConfirm from "./function/useConfirm/index";
+import usePreventLeave from "./function/usePreventLeave/index" 
 
 const content = [
   {
@@ -17,6 +19,10 @@ const content = [
   }
 ];
 const App =() => {
+  //confirm
+  const deletWord = () => console.log("삭제")
+  const protectWord = () => console.log("ss")
+  const confirmDeletWord = useConfirm("ok?", deletWord, protectWord)
   //Title
   const titleUpdate = useTitle("Loading");
   setTimeout(() => titleUpdate("HOME"), 5000)
@@ -26,7 +32,6 @@ const App =() => {
     document.getElementById("demo").innerHTML = "눌러짐";
   }
   const clickUpdate = useClick(clicked);
-
   //tab
   const {currentItem, changeItem} = useTab(0,content);
   //input
@@ -37,10 +42,12 @@ const App =() => {
 
   return (
     <div>
-
     <div>
       <input placeholder="Hello" {...name} />
       <h1 id="demo" ref={ clickUpdate }>i want Click for you</h1>
+      <button onClick={confirmDeletWord}>delet all</button>
+      <button onClick={usePreventLeave.enablePrevent}>protect</button>
+      <button onClick={usePreventLeave.disablePrevent}>unprotect</button>
     </div>
 
     <div>
